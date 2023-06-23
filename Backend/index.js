3e8d5987651fc1c80390af803bc065e87e8b6dd0
Abhimanyu <lp.abhimanyu@gmail.com>
2023-06-23 12:21:04 +0530

    Added readme for backend / minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,18 +3,27 @@ require("dotenv").config();
 const {Configuration, OpenAIApi} = require("openai")
 
 const app = express();
-
 app.use(express.json());
 
+
+//OpenAI Config
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration)
 
+
+//Routes
 app.get("/", (req, res) => {
     res.status(200).send("Home Page of Backend")
 })
+
+
+//For Frontend - keep body in this json format:
+// {
+//     "prompt":"your query"
+// }
 
 app.post("/query", async (req, res) => {
 
@@ -28,7 +37,7 @@ app.post("/query", async (req, res) => {
 
         return res.status(200).json({
             success: true,
-            data: response.data.choices[0]
+            data: response.data.choices[0].text
         })
     } catch (error) {
         return res.status(400).json({
@@ -40,6 +49,8 @@ app.post("/query", async (req, res) => {
     }
 })
 
+
+//Server Running
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
