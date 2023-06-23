e3ab39246ac53076dc28e6505a3c3664eebb014d
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-23 22:57:27 +0530

    updated code from vipul

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
+const socketio = require("socket.io");
 const cors = require("cors");
 const { UserRoute } = require("./Routes/Users.Routes");
 const { connection } = require("./config/db");
@@ -63,114 +64,53 @@ app.post("/query", async (req, res) => {
   }
 });
 
-const obj = [
-  {
-    question: "What is JavaScript?",
-    answer: "JavaScript is a server-side language",
-  },
-  {
-    question: "What is HTML?",
-    answer: "HTML is a programming language",
-  },
-];
-
-console.log(obj);
-//   let objStr = JSON.stringify(obj);
-
 app.post("/rating", async (req, res) => {
-  // const { qn1 , qn2 , ans1, ans2 } = req.body;
+  // Extract Question 1 and Answer 1 from req.body
+  const question1 = req.body.question1;
+  const answer1 = req.body.answer1;
 
-  // Set the prompt based on the received job role and experience
-  const prompt = `read the following array of objects and rate the answer out of 10 according to their questions. 
+  // Extract Question 2 and Answer 2 from req.body
+  const question2 = req.body.question2;
+  const answer2 = req.body.answer2;
+
+  // Set the prompt based on the received job role and experience, including the extracted values
+  const prompt = `Rate Each Answers out of 10 given to these below questions and justification(1 line) why you rated this particular number.
+
+	Question 1: ${question1}
+	Answer 1: ${answer1}
 	
-	${obj.toString()} 
-	
-	Provide an object in that object there will be only two data, first one will be the overall score in number and the second one will be the justification`;
+	Question 2: ${question2}
+	Answer 2: ${answer2}`;
+  // console.log(prompt);
 
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      max_tokens: 500,
-      temperature: 1, // Adjust the value to control the randomness of the generated text
-      //   stop: "\n",
+      max_tokens: 1000,
+      temperature: 0.7, // Adjust the value to control the randomness of the generated text
+      // stop: "\n",
     });
 
-    // let data = response.data.choices[0].text
-    // let stringWithoutNewlines = data.replace(/\n\n/g, "");
-
-    // let qnArray = stringWithoutNewlines.split("\n")
-    // console.log(qnArray)
     let data = response.data.choices[0].text;
+
     console.log(data);
 
     return res.status(200).json({
       success: true,
-      data: response.data.choices[0].text,
+      data: data,
     });
   } catch (error) {
     return res.status(400).json({
       success: false,
       error: error.response
         ? error.response.data
+          ? error.response.data.error.message
+          : "There was an issue on the server"
         : "There was an issue on the server",
     });
   }
 });
-
-app.post("/rating", async (req, res) => {
-	// Extract Question 1 and Answer 1 from req.body
-	const question1 = req.body.question1;
-	const answer1 = req.body.answer1;
-
-	// Extract Question 2 and Answer 2 from req.body
-	const question2 = req.body.question2;
-	const answer2 = req.body.answer2;
-
-	// Set the prompt based on the received job role and experience, including the extracted values
-	const prompt = `Rate Each Answers out of 10 given to these below questions and justification(1 line) why you rated this particular number.
-
-	Question 1: ${question1}
-	Answer 1: ${answer1}
-	
-	Question 2: ${question2}
-	Answer 2: ${answer2}`
-	;
-
-	// console.log(prompt);
-
-	try {
-		const response = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt,
-			max_tokens: 1000,
-			temperature: 0.7, // Adjust the value to control the randomness of the generated text
-			// stop: "\n",
-		});
-
-		let data = response.data.choices[0].text;
-
-		console.log(data)
-
-
-		return res.status(200).json({
-			success: true,
-			data: data,
-		});
-	} catch (error) {
-		return res.status(400).json({
-			success: false,
-			error: error.response
-				? error.response.data
-					? error.response.data.error.message
-					: "There was an issue on the server"
-				: "There was an issue on the server",
-		});
-	}
-});
-
-
-
 
 //Server Running
 const port = process.env.PORT || 5000;
