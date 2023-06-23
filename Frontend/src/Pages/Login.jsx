c97d9c658c02c9f9a9769566105a75fd2fbe608c
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-23 20:00:30 +0530

    added verify route in backend and fixed some small bugs in frontend

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,55 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../css/login.css";
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const payload = {email,password}
-        axios.post(`http://localhost:5000/user/login`, payload)
-            .then((res) => {
-                console.log(res.data)
-                alert(res.data.msg)
-                // navigate("/login")
-            })
-            .catch((err) => {
-                console.log(err.message)
-                alert(err.message)
-            })
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const payload = { email, password };
+    axios
+      .post(`http://localhost:5000/user/login`, payload)
+      .then((res) => {
+        console.log(res.data);
+        alert(res.data.msg);
+        // navigate("/login")
+      })
+      .catch((err) => {
+        console.log(err.message);
+        alert(err.message);
+      });
+  };
 
-    return (
-        <div id='container'>
-
-            <div id='login-card'>
-                <h2 >Please Login</h2>
-                {/* <img src="" alt="" srcset="" /> */}
-                <div>
-                    <form onSubmit={handleLogin}>
-                        <label >Username</label>
-                        <br />
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" />
-                        <br />
-                        <label>Password</label>
-                        <br />
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" />
-                        <br />
-                        <button type="submit">Login</button>
-                    </form>
-                    <Link to="/forgot-password">
-                        <h5>Forgot password?</h5>
-
-                    </Link>
-                </div>
-            </div>
-            <div>
-                <p> Don't have account?  <Link to={"/sign-up"}>
-                    SignUp</Link></p>
-            </div>
+  return (
+    <div id="container">
+      <div id="login-card">
+        <h2>Please Login</h2>
+        {/* <img src="" alt="" srcset="" /> */}
+        <div>
+          <form onSubmit={handleLogin}>
+            <label>Username</label>
+            <br />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+            />
+            <br />
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+            />
+            <br />
+            <button type="submit">Login</button>
+          </form>
+          <Link to="/resetPass">
+            <h5>Forgot password?</h5>
+          </Link>
         </div>
-    )
-}
+      </div>
+      <div>
+        <p>
+          {" "}
+          Don't have account? <Link to={"/signup"}>SignUp</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
---
+++ Frontend/src/Redux/actionTypes.js
@@ -0,0 +1,1 @@
+export const USER_AUTH = "USER_AUTH";
