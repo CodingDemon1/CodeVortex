7fb4e8624a5ff3fbd89c8b1bf2bd391d294a4c46
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-23 13:35:00 +0530

    interview screen

---
+++ Frontend/src/component/Question.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = () => {
+  return (
+    <li className=" text-xl font-medium	">
+      Here is the sample question that will be asked?
+    </li>
+  );
+};
+
+export default Question;
